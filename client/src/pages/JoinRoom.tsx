6454c51c8cce8a9f3306f8e302f6e54cd72cca7b
gajdoczi <pelda@gmail.com>
2026-07-06 11:36:05 +0200

    Add Join Room page

--- client/src/pages/JoinRoom.tsx
+++ client/src/pages/JoinRoom.tsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import "./JoinRoom.css";
+
 function JoinRoom() {
   return (
-    <div>
-      <h1>Join Room</h1>
+    <div className="join-room">
+      <div className="card">
+        <h1>Join Room</h1>
+
+        <input
+          type="text"
+          placeholder="Your Name"
+        />
+
+        <input
+          type="text"
+          placeholder="Room Code"
+        />
+
+        <button>Join</button>
+
+        <Link to="/">
+          <button className="back">
+            Back
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
